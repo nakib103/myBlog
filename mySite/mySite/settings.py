73d831c54b395b2a656f5f5eb346b1e977e89a38
Syed Nakib Hossain <snhossain@ebi.ac.uk>
2023-02-19 21:47:00 +0000

    Move static files to doc for github page deployment

--- mySite/mySite/settings.py
+++ mySite/mySite/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'docs')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
